88bc98d015a2c97486efb6452852e3c4698a0b35
Arthur Murillo <arthurmurillo9@gmail.com>
2020-10-01 15:55:42 +0200

    Préparation pour statistiques évaluations simples

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,20 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Repository\GroupeEtudiantRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/api")
  */
 class ApiController extends AbstractController
 {
+    private $serializer;
+    private $groupeEtudiantRepository;
+    private $squeletteTableauRetour;
+
+    public function __construct(SerializerInterface $serializer, GroupeEtudiantRepository $groupeEtudiantRepository) {
+        $this->serializer = $serializer;
+        $this->groupeEtudiantRepository = $groupeEtudiantRepository;
+        $this->squeletteTableauRetour = [
+            'code' => 0,
+            'errors' => [],
+            'data' => [
+                'type' => '',
+                'statisticsData' => []
+            ]
+        ];
+    }
+
     /**
-     * @Route("/", name="index")
+     * @Route("/statistiques/evaluationSimple", name="api_get_stats_eval_simple", methods={"GET"})
      */
-    public function index()
+    public function getStatistiquesSimples(Request $request)
     {
-        return new Response("coucou");
+        //Preparation tableau renvoyé
+        $returnArray = $this->squeletteTableauRetour;
+        $returnArray['data']['type'] = 'evaluationSimple';
+
+        //Récupération et vérification des paramètres
+
+        //Calcul
+
+        //Renvoi des statistiques
+
+        return new Response($this->serializer->serialize($returnArray, 'json'));
     }
 }
