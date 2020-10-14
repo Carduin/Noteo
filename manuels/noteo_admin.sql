7801ba5d56743202e0c6c84da0dcef7514512dfe
Arthur Murillo <arthurmurillo9@gmail.com>
2020-10-14 17:40:41 +0200

    Suppression d'un fichier devenu inutile dans cette version

--- manuels/noteo_admin.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `enseignant` (`id`, `email`, `roles`, `password`, `nom`, `prenom`)
-VALUES (1, 'noteo@admin.fr', '[\"ROLE_USER\",\"ROLE_ADMIN\"]', '$2y$10$DzfLbLyjpDUiLyYlPcw.L.RqtdOxxS7XCuXeg.bq3Glu5gG9W04WO', 'Noteo', 'Admin');
