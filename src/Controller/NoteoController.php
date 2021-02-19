ece6d63d030739eefd74aa215639f2d8e53cbe92
Arthur Murillo <arthurmurillo9@gmail.com>
2021-02-19 22:14:13 +0100

    Fix reset

--- src/Controller/NoteoController.php
+++ src/Controller/NoteoController.php
@@ -2,10 +2,8 @@
 
 namespace App\Controller;
 
-use App\Repository\ApiLogRepository;
 use App\Repository\EtudiantRepository;
 use App\Repository\GroupeEtudiantRepository;
-use App\Repository\ShortenedURLRepository;
 use App\Repository\StatutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,35 +27,9 @@ class NoteoController extends AbstractController
     }
 
     /**
-     * @Route("/{_locale}/historique-api", name="historique_api")
-     */
-    public function historiqueApi(ApiLogRepository $repoAPI)
-    {
-        $this->denyAccessUnlessGranted("API_HISTORY", $this->getUser());
-        return $this->render('API/historique_api.html.twig', [
-            'logs' => $repoAPI->getAllLogsWithEnseignants()
-        ]);
-    }
-
-    /**
-     * @Route("/{_locale}/historique-api/supprimer", name="empty_historique_api")
-     */
-    public function viderHistoriqueApi(ApiLogRepository $repoAPI)
-    {
-        $this->denyAccessUnlessGranted("API_HISTORY", $this->getUser());
-        $em = $this->getDoctrine()->getManager();
-        $logs = $repoAPI->findAll();
-        foreach ($logs as $log) {
-            $em->remove($log);
-        }
-        $em->flush();
-        return $this->redirectToRoute('historique_api');
-    }
-
-    /**
      * @Route("/reinitialiser", name="app_reset")
      */
-    public function reinitialiserApplication(GroupeEtudiantRepository $repoGroupe, StatutRepository $repoStatut, EtudiantRepository $repoEtudiant, ApiLogRepository $logRepository, ShortenedURLRepository $shortenedURLRepository)
+    public function reinitialiserApplication(GroupeEtudiantRepository $repoGroupe, StatutRepository $repoStatut, EtudiantRepository $repoEtudiant)
     {
         $this->denyAccessUnlessGranted("RESET_APPLICATION", $this->getUser());
         $entityManager = $this->getDoctrine()->getManager();
@@ -78,12 +50,6 @@ class NoteoController extends AbstractController
         }
         foreach ($repoEtudiant->findAll() as $etudiant) {
             $entityManager->remove($etudiant);
-        }
-        foreach ($logRepository->findAll() as $log) {
-            $entityManager->remove($log);
-        }
-        foreach ($shortenedURLRepository->findAll() as $shortURL) {
-            $entityManager->remove($shortURL);
         }
         $entityManager->flush();
         return $this->redirectToRoute("groupe_etudiant_index");
